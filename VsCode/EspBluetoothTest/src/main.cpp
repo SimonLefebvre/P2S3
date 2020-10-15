e12386b499647ebd50787d8d6a8edf64f8e33626
SimonLefebvre <55550070+SimonLefebvre@users.noreply.github.com>
2020-10-15 10:02:56 -0400

    display changes + pwm tested

--- VsCode/EspBluetoothTest/src/main.cpp
+++ VsCode/EspBluetoothTest/src/main.cpp
@@ -12,9 +12,23 @@ void setup() {
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   //display.setLogBuffer(5, 30);
   display.drawStringMaxWidth(0, 0, 128,
-      "ma name a jeff" );
+      "U Moteur 24V" );
+  display.drawStringMaxWidth(0, 16, 128,
+  "U Gene 80V" );
+  display.display();
+  display.drawStringMaxWidth(0, 32, 128,
+  "T Moteur 20\248C" );
+  display.display();
+  display.drawStringMaxWidth(0, 48, 128,
+  "T Gene 80\248C" );
   display.display();
   Serial.println("Display started");
+
+  pinMode(0,OUTPUT);
+  ledcAttachPin(16,0);//ledcAttachPin(gpio, channel)
+  //ledcSetup(0,1000000,10);//ledcSetup(channel, frequency, resolution)
+  //ledcWrite(0,0.25*1024);//ledcWrite(channel, dutycycle)
+  Serial.println("PWM started");
 }
 
 void loop() {
